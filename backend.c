e71b5ada8b7999924587aee1250dce5ac43e4309
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-10-12 21:45:27 +0300

    Initial

---
+++ backend.c
@@ -0,0 +1,51 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <err.h>
+#include <strings.h>
+#include <stdio.h>
+#include <unistd.h>
+
+const char reply[] =
+	"HTTP/1.1 200 OK\r\n"
+	"Content-type: text/plain\r\n"
+	"\r\n"
+	"OK\r\n";
+
+int main() {
+	int s;
+
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+		err(1, "socket");
+
+	struct sockaddr_in sin;
+	bzero(&sin, sizeof(sin));
+	sin.sin_len = sizeof(sin);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(8081);
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(s, (struct sockaddr*)&sin, sizeof(sin)) < 0)
+		err(1, "bind");
+
+	if (listen(s, 4) < 0)
+		err(1, "listen");
+
+	char buf[65536];
+	while (1) {
+		int s2 = accept(s, NULL, NULL);
+		if (s2 < 0)
+			err(1, "accept");
+
+		if (recv(s2, buf, 65536, 0) < 0)
+			err(1, "recv");
+
+		usleep(1500000); // 1.5 sec
+
+		if (send(s2, reply, sizeof(reply), 0) < 0)
+			err(1, "send");
+
+		close(s2);
+	}
+
+	return 0;
+}
---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if ! [ -e backend -a backend -nt backend.c ]; then
+	echo "===> Compiling backend simulator"
+	cc -o backend backend.c
+fi
+
+echo "===> Running backend simulator"
+
+./backend &
+backend_pid="$!"
+
+echo "===> Running nginx"
+
+nginx -p . -c nginx.conf >/dev/null 2>&1 &
+nginx_pid="$!"
+
+echo "===> Waiting 1 sec for everybody to start"
+
+sleep 1
+
+kill -0 "$backend_pid" >/dev/null 2>&1 || { echo "backend seem to have failed to start"; kill "$nginx_pid" >/dev/null 2>&1; exit 1; }
+kill -0 "$nginx_pid" >/dev/null 2>&1 || { echo "nginx seem to have failed to start"; kill "$backend_pid" >/dev/null 2>&1; exit 1; }
+
+echo "===> Trying request"
+
+time curl --silent http://localhost:8080 >/dev/null
+
+echo "/!\\ The backend replies in 1.5 seconds"
+echo "/!\\ If you see times > 2 seconds here, you're experiencing nginx problem"
+
+echo "===> Shutting everything down"
+
+kill "$backend_pid"
+kill "$nginx_pid"
+
+wait
